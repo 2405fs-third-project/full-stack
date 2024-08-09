6259e20186875e0e727c4fa4f127997df83f4245
heetae <chtae0314@naver.com>
2024-08-10 00:38:56 +0900

    Save current changes before switching branches

---
+++ frontend/src/components/Main/MainForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MainForm.css";
+
+const MainForm = () => {
+  return (
+    <div className="main_wrap">
+      <div className="main">메인페이지</div>
+    </div>
+  );
+};
+
+export default MainForm;
