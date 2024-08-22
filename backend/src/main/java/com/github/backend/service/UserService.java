e121ff34e18171aa7c9ed413a9ee2e2f9462947d
jiyoung choi <codingold@naver.com>
2024-08-22 15:39:46 +0900

    complete updateUserRole function

--- backend/src/main/java/com/github/backend/service/UserService.java
+++ backend/src/main/java/com/github/backend/service/UserService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.Role;
 import java.util.Optional;
 
 @Service
@@ -32,7 +33,36 @@ public class UserService {
             return MessageService.SUCCEED_CREATE_ACCOUNT.getMessage();
         }
 
+    public void updateUserRole(Integer point,String userId){
+        User user= userRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException(MessageService.USER_NOT_FOUND.getMessage()));
+        Role role;
+        if (point >= 500) {
+            role = Role.LEVEL5;
+        } else if (point >= 400) {
+            role = Role.LEVEL4;
+        } else if (point >= 300) {
+            role = Role.LEVEL3;
+        } else if (point >= 200) {
+            role = Role.LEVEL2;
+        } else  {
+            role = Role.LEVEL1;
+        }
+        user.setRole(role);
+        user.setPoint(point);
+        userRepository.save(user);
     }
+    public enum Role {
+        ADMIN,
+        LEVEL5,
+        LEVEL4,
+        LEVEL3,
+        LEVEL2,
+        LEVEL1
+    }
+}
+
+
 
 
 
