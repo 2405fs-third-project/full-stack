0c3040b4da18505ef9b05d39b20dcc3c5e655027
jiyoung choi <codingold@naver.com>
2024-08-27 10:15:55 +0900

    CorsConfig file, changes

--- backend/src/main/java/com/github/backend/config/CorsConfig.java
+++ backend/src/main/java/com/github/backend/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://13.125.98.63:3000","http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .exposedHeaders("Authorization");
     }
 }
